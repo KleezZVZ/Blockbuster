b38200aca210ccb9a62e039af514e7f2e9832127
Mario Márquez <mariomarquez.0116@gmail.com>
2023-12-11 10:20:52 -0400

    Refactor: Eliminado la libreria de Windows.h.

--- src/Blockbuster.cpp
+++ src/Blockbuster.cpp
@@ -1,8 +1,7 @@
-#include <Windows.h>
+#include <stdlib.h>
 #include "view/view.h"
 using namespace std;
 int main(){
-    system("color 4f");
     menu_principal();
     system("pause");
     return 0;
